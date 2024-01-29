ed5ca3bc1672492d59362cc13b51ac41b27aa3c9
evelinag <evelina@evelinag.com>
2024-01-29 14:51:44 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# Demo - structured missingness explainer
+
+## Prerequisites:
+
+- Node.js
+- Typescript: `npm install --save-dev typescript`
+- http-server: `npm install -g http-server`
+
+## Build:
+
+1. Install dependencies
+```
+npm install
+```
+
+2. Compile the TypeScript file to JavaScript:
+
+```bash
+npx tsc
+```
+
+3. Run a local server to preview 
+```bash
+http-server
+```
+in the folder that contains `index.html`. Navigate to `http://localhost:8080` in the browser.
+
---
+++ index.ts
@@ -0,0 +1,60 @@
+let currentStoryPart = 'start';
+
+window.onload = () => {
+    const button = document.getElementById('changeTextButton');
+    const dataElement = document.getElementById('dataDisplay');
+    const restartButton = document.getElementById('restartButton');
+
+    if (button && dataElement && restartButton) {
+        // Add Bulma classes to the button
+        button.className = "button is-primary"
+        restartButton.className = "button is-danger is-rounded";
+
+        button.addEventListener('click', () => {
+            fetch('data/demo.json')
+                .then(response => {
+                    if (!response.ok) {
+                        throw new Error('Network response was not ok');
+                    }
+                    return response.json();
+                })
+                .then(data => {
+                    // Display the current part of the story
+                    const storyPart = data.story[currentStoryPart];
+                    dataElement.innerText = storyPart.text;
+
+                    // Create buttons for the choices
+                    const choicesElement = document.getElementById('choices');
+                    if (choicesElement) {
+                        choicesElement.innerHTML = ''; // Clear previous choices
+
+                        const buttonWrapper = document.createElement('div');
+                        buttonWrapper.className = "buttons";
+                        choicesElement.appendChild(buttonWrapper);
+
+                        storyPart.choices.forEach((choice: { text: string, next: string }) => {
+                            const choiceButton = document.createElement('button');
+                            choiceButton.innerText = choice.text;
+                            choiceButton.className = "button is-link";
+                            choiceButton.addEventListener('click', () => {
+                                currentStoryPart = choice.next;
+                                button.click(); // Trigger the next part of the story
+                            });
+                            buttonWrapper.appendChild(choiceButton);
+                        });
+                    }
+                })
+                .catch(error => {
+                    console.error('An error occurred while attempting to fetch the JSON file:', error);
+                });
+            });
+
+        restartButton.addEventListener('click', () => {
+            currentStoryPart = 'start';
+            button.click(); // Trigger the start of the story
+        });
+
+        // Trigger the start of the story
+        button.click();
+    }
+};
